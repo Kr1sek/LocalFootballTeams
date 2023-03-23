923cf987b9151ed108c8348fab0d32e36e450c8d
Kr1sek <krystiamoskal@gmail.com>
2023-03-23 10:31:37 +0100

    Add project files.

---
+++ LocalFootballTeam.Models/Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalFootballTeam.Models.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Addres { get; set; } = string.Empty;
+        public string StartYear { get; set; } = string.Empty;
+        public string Logo { get; set; } = string.Empty;
+
+    }
+}
