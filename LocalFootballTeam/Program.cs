4da020920806ddea7b3d80b12fd06fc9df2df32d
Kr1sek <krystiamoskal@gmail.com>
2023-04-01 13:35:20 +0200

    Add Cors Policy

--- LocalFootballTeam/Program.cs
+++ LocalFootballTeam/Program.cs
@@ -2,11 +2,15 @@ using LocalFootballTeam.Services.Services;
 using LocalFootballTeam.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Forms;
 using LocalFootballTeam.Migrations;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseHttpsRedirection();
 
